d945e3041de835fcdf6eaa96a5141e64133bfc12
pannchat <pannchat@likelion.org>
2020-10-18 21:54:29 +0900

    SCSS

--- project3/gobackn/src/App.js
+++ project3/gobackn/src/App.js
@@ -1,63 +1,117 @@
+/* eslint-disable */
 import React from 'react';
 import Sender from './Sender';
 import Receiver from './Receiver';
 import './App.css';
+let N = 10;
+let pk = 1000;
+let base = 1;
+let seqn = 1;
+let runWindow = false;
+let windows = [];
 
 let receiverQ = [];
 let rcvpkt = [];
-let i,j;
+let buf = [];
+let i;
 let seq, data;
 let rcvV = 1;
+let Timeout = [];
 function channel(who, seq, data) {
-  if (who === 'sender'){
-    if ((Math.random()) < 0.1){
-      console.log("send error" + seq +"and"+ data)
-    }else{
-      rcv(seq,data)
+  Timeout[seq] = new Date();
+  if (who === 'sender') {
+    if (Math.random() < 0.1) {
+      console.log('---------- send error' + seq + 'and' + data + '----------');
+    } else {
+      console.log('sender : seq' + seq + ' 전송 완료.');
+      rcv('rcv', seq, data);
     }
-  }else if(who === 'resender'){
-    console.log([seq]+"재전송 됨")
-    rcv(seq,data)
+  } else if (who === 'resender') {
+    console.log([seq] + '재전송 됨');
+    rcv('resend', seq, data);
+  } else if (who === 'receiver') {
+    // rcv('rcv', seq, data);
+    receiverQ.push([seq, data]);
   }
 }
-function rcv(seq, data ) {
-  console.log([seq, data] + "전송받음. ACK발송") 
-  if (seq === rcvV){
-    
-    receiverQ.push([seq,data]);
-    rcvV++;
+function rcv(state, seq, data) {
+  if (state === 'rcv') {
+    if (seq === rcvV) {
+      channel('receiver', seq, data);
+      // receiverQ.push([seq, data]);
+      console.log('receiver application: ' + seq + '수신 완료');
+      rcvV++;
+    } else {
+      channel('receiver', seq, data);
+      // receiverQ.push([seq, data]);
+      buf.push([seq, data]);
+      console.log('????순서 안맞아 : ' + buf, rcvV);
+    }
+  } else if (state === 'resend') {
+    receiverQ.unshift([seq, data]);
+    buf.push([seq, data]);
+    buf.sort((a, b) => a[0] - b[0]);
+    try {
+      while (true) {
+        // console.log('while 실행햇어 ㅡㅡ<<<<', buf[0][0] + 1 === buf[1][0]);
+        // if (buf[1] !== undefined) {
+        if (buf[0][0] + 1 === buf[1][0]) {
+          // console.log(buf[i][0] + ' while ' + seq);
+
+          console.log(
+            'receiver application(buffer) : ' + buf.shift()[0] + ' 수신 완료?',
+          );
+
+          console.log('buffer' + buf);
+          rcvV++;
+        } else if (buf[0] === pk) {
+          console.log(
+            'receiver application(buffer) : ' + buf.shift()[0] + ' 수신 완료?',
+          );
+          console.log('break;');
+          break;
+        } else {
+          console.log('break;');
+          break;
+        }
+      }
+    } catch {
+      console.log(
+        'receiver application(buffer) : ' + buf.shift()[0] + ' 수신 완료?',
+      );
+      rcvV++;
+    }
   }
 }
 
 function App() {
-
   (function snd() {
-    var pk = 1000;
     var packt = new Array(pk);
     for (var i = 0; i < pk; i++) {
-      packt[i] = i;
+      packt[i] = '!';
     }
     // console.log(packt);
-    let N = 10;
-    let base = 1;
-    let seqn = 1;
-    let runWindow = false;
-    let windows = [];
-    
+
     while (!runWindow || windows.length !== 0) {
-      while(base + N > seqn && !runWindow) {
+      while (base + N > seqn && !runWindow) {
         var sndpkt = [];
         sndpkt.push(seqn);
         sndpkt.push(packt.shift());
-        console.log(sndpkt, packt.length);
-        
+        // console.log(sndpkt, packt.length);
+
         windows.push(sndpkt); //[1~]
-        channel("sender",sndpkt[0], sndpkt[1]);
+        console.log(sndpkt);
+        channel('sender', sndpkt[0], sndpkt[1]);
 
         // socket.emit('init', { seq: sndpkt[0], data: sndpkt[1] });
-        console.log('send packet seq N = ', seqn, 'windows', windows);
+        console.log(
+          'send packet seq N = ',
+          seqn,
+          'windows.length',
+          windows.length,
+        );
         seqn++;
-        console.log(windows);
+
         if (packt.length === 0) {
           runWindow = true;
           console.log('end');
@@ -67,40 +121,51 @@ function App() {
         // if (seqn === 10) runWindow = true;
       }
 
-        
-        try{
-          console.log('ACK'+receiverQ[0][0] + "수신")
-        if (receiverQ[0][0] == windows[0][0]){ //같으면
-          if(receiverQ[0][0] === 1000){
-            console.log("프로그램 종료")
-            break;
-          }
-          receiverQ.shift();
+      try {
+        // if (receiverQ[0][0] !== undefined) {
+
+        let Ack = receiverQ[0][0];
+        if (Ack == windows[0][0]) {
+          console.log('sender : ACK' + receiverQ.shift()[0] + '수신');
+
           windows.shift();
+
           base++;
-          console.log("base 증가>> "+base);
-        }
-        }catch{
-          receiverQ = [];
-          for(i in windows){
-            console.log(windows[i] +" 패킷에 대하여 재전송")
-            // alert("retransmit seq:"+windows[i][0])
-            channel("resender",windows[i][0], windows[i][1]  )
-            // receiverQ.push(windows[i]);
+        } else {
+          var nowDate = new Date();
+          if (nowDate - Timeout[receiverQ[0][0]] > 150) {
+            // alert('timeout 재발송');
+            console.log(windows[0], '재발송했어');
+            channel('resender', windows[0][0], windows[0][1]);
           }
         }
-        
-      
+        // } else {
+        //   console.log('시발련아');
+        // }
+      } catch {
+        if (receiverQ.length === 0 && windows[0][0] === pk) {
+          var nowDate = new Date();
+          // alert('timeout 재발송');
+          console.log(windows[0][0], '재발송했어 catch');
+          channel('resender', windows[0][0], windows[0][1]);
+          console.log(
+            'sender : ACK',
+            receiverQ.shift()[0],
+            '수신\n캐치함 프로그램 종료',
+          );
+          break;
+        }
+        // var nowDate = new Date();
+        // if (nowDate - Timeout > 150) {
+        //   alert('timeout 재발송');
+        // }
+      }
     }
-  
   })();
-  
+
   return (
     <div>
-      <div>
-        console 확인해주세요
-
-      </div>
+      <div>console 확인해주세요</div>
     </div>
   );
 }
