b69971614279cf530792ffcf3365f0131fb1b8c5
pannchat <pannchat@likelion.org>
2021-07-29 14:45:19 +0900

    test

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,15 +1,7 @@
 import React, { useState, useRef } from 'react';
 
 function InputSample(){
-    const [inputs, setInputs] = useState({
-        tankWidth: '',
-        tankHeight: '',
-        tankDepth: '',
-        tankSand: '',
-        waterLevel: '',
-        tankWeight: '',
-        capacity: '내 수조의 용량을 계산해보세요.',
-    })
+
     const {name, nickname} = inputs;
     const nameInput = useRef();
     const onChange = (e) => {
@@ -27,10 +19,10 @@ function InputSample(){
         })
         nameInput.current.focus();
     }
-    const { tankWidth, tankDepth, tankHeight } = inputs;
+
     return (
         <>
-        <input name="tankWidth" onChange={onChange} value={tankWidth}/>
+        <input name="tankWidth" onChange={onChange}/>
         <input name="nickname" onChange={onChange} value={nickname}/>
         <button onClick={onReset}>초기화</button>
         <p>{name}:({nickname})</p>
